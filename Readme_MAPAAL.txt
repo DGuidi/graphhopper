1c84328db050dad1a4baeab96f504974b31b88a3
Marco Zuccaroli <marcozuccaroli@gmail.com>
2014-10-31 17:39:07 +0100

    #3779 impostate scadenze per le segnalazioni

--- Readme_MAPAAL.txt
+++ Readme_MAPAAL.txt
@@ -1,5 +1,11 @@
+Documentazione:
+- database: https://docs.google.com/document/d/1Ripv2BZaQqfqNZcVWYnhn-Ak0a8yr-AJVahnBPpJNnA/edit?usp=sharing
+- MAPAAL rest API: https://docs.google.com/document/d/1hKmMzM1t8IIxPDbaNXD0MSoRngzH9vXZgo-PAkV0hIE/edit?usp=sharing
+- Obstacles rest API: https://docs.google.com/document/d/1nKOp0fhF8N7hI9F9NcboQkbFwzEjj-NVGuHwUOE5eB0/edit?usp=sharing
+
+AVVIARE IL SERVER
 avviare il file test.cmd che provvederà a fare partire l'applicazione di test sull'osm di Ancona presente nel pacchetto
 
-per visualizzare un interfaccia di prova andare all'indirizzo http://localhost:8989/?point=43.607152%2C13.498056&point=43.614897%2C13.504633&vehicle=mapaal&locale=it-IT
-
 per avviare i test automatici andare all'indirizzo http://localhost:8989/tests.html 
+
+per visualizzare un interfaccia di test andare all'indirizzo http://localhost:8989/?point=43.607152%2C13.498056&point=43.614897%2C13.504633&vehicle=mapaal&locale=it-IT
--- web/src/main/webapp/js/tests.js
+++ web/src/main/webapp/js/tests.js
@@ -2,8 +2,7 @@ $(function () {
   var host, createDefaultRequest,createReroutingRequest, decodePath, formatCoord, formatInstruction, url, jsondata;
 
   host = '/';
-  
-  
+ 
   createurl = function(pointALat,pointALon,pointBLat,pointBLon){
 	  return[
        host + 'route?',
@@ -14,29 +13,33 @@ $(function () {
      ].join('&');
   };
 
-  createJsonData = function(nodeA,nodeB,lat,lon){
-  jsonData=[{
-	    "nodes": [nodeA,nodeB],
-	    "poi": {
-	      "type": "Feature",
-	      "geometry": {
-	        "type": "Point",
-	        "coordinates": [lat,lon]
-	      },
-	      "properties": {
-	        "typename": "POI"
-	      }
-	    },
-	    "details": {
-	      "sensor": {
-	        "type": "test automatico",
-	        "info": "segnalazione da test automatico",
-	        "details": "segnalazione da test automatico, more data"
-	      }
-	    }
-	  }];
-  return JSON.stringify(jsonData);
-  };
+  	createJsonData = function(nodeA, nodeB, lat, lon, duration) {
+		jsonData = [ {
+			"nodes" : [ nodeA, nodeB ],
+			"poi" : {
+				"type" : "Feature",
+				"geometry" : {
+					"type" : "Point",
+					"coordinates" : [ lat, lon ]
+				},
+				"properties" : {
+					"typename" : "POI"
+				}
+			},
+			"details" : {
+				"sensor" : {
+					"type" : "test automatico",
+					"info" : "segnalazione da test automatico",
+					"details" : "segnalazione da test automatico, more data"
+				}
+			}
+		} ];
+
+		if (duration != null) {
+			jsonData[0].duration = duration
+		}
+		return JSON.stringify(jsonData);
+	};
 
 	createDefaultRequest = function (url) {
 	  return $.ajax({
@@ -109,157 +112,154 @@ $(function () {
 	  return coord[0].toFixed(6) + ' ' + coord[1].toFixed(6);
 	};
 
-	formatInstruction = function (instr) {
-	  var nodes = instr.nodes || [0, 0];
-	  return [instr.text, ': [', nodes[0] || 'null', '-', nodes[1] || 'null', ']'].join('');
+	formatInstruction = function(instr) {
+		var nodes = instr.nodes || [ 0, 0 ];
+		return [ instr.text, ': [', nodes[0] || 'null', '-',
+				nodes[1] || 'null', ']' ].join('');
 	};
-	
-	asyncTest("Repository Janitor", function () {
-		    expect(2);
-		    var req = createDefaultRequest( host + 'repoJanitor')
-					.done(function (json) {
-					  var info, cleanedRows;
-					  info = json.info;
-					  cleanedRows= json.cleanedRows;
-					  console.log(json);
-					  ok(info, 'request completed in: ' + info.took);
-					  ok(cleanedRows>=0, 'rows cleaned : ' + cleanedRows);
-					});
-		  });
-	
 
-  asyncTest("default routing - via mamiani", function () {
-    expect(6);
-    var url = createurl(43.612966,13.50526,43.611856,13.504794);
-    var expetedLenght=4
-    
-    var req = createDefaultRequest(url)
-			.done(function (json) {
-			  var info, paths, path;
-
-        info = json.info;
-				paths = json.paths;
+	asyncTest("Repository Janitor", function() {
+		expect(2);
+		var req = createDefaultRequest(host + 'repoJanitor').done(
+			function(json) {
+				var info, cleanedRows;
+				info = json.info;
+				cleanedRows = json.cleanedRows;
 				console.log(json);
-			  ok(info, 'request completed in: ' + info.took);
-			  ok(paths && paths.length && paths.length === 1, 'paths valid');
-			  path = paths[0];
-			  ok(path, 'path found');
-
-			  ok(!path.points_encoded, 'points are not encoded');
-			  var points = path.points.coordinates;
-			  ok(points, 'data: ' + _(points).map(formatCoord).join(','));
-
-			  var instructions = path.instructions;
-			  ok(instructions && instructions.length === expetedLenght, 'instructions: ' + _(instructions).map(formatInstruction).join(','));
+				ok(info, 'request completed in: ' + info.took);
+				ok(cleanedRows >= 0, 'rows cleaned : ' + cleanedRows);
 			});
-  });
+	});
+
+	asyncTest("default routing - via mamiani", function() {
+		expect(6);
+		var url = createurl(43.612966, 13.50526, 43.611856, 13.504794);
+		var expetedLenght = 4
+		var req = createDefaultRequest(url).done(
+			function(json) {
+				var info, paths, path;
+				info = json.info;
+				paths = json.paths;
+				ok(info, 'request completed in: ' + info.took);
+				ok(paths && paths.length && paths.length === 1,'paths valid');
+				path = paths[0];
+				ok(path, 'path found');
+				ok(!path.points_encoded, 'points are not encoded');
+				var points = path.points.coordinates;
+				ok(points, 'data: '+ _(points).map(formatCoord).join(','));
+				var instructions = path.instructions;
+				ok(instructions&& instructions.length === expetedLenght,'instructions: '+ _(instructions).map(formatInstruction).join(','));
+			});
+	});
 
  
-  asyncTest("re-routing request - via mamiani", function () {
-    expect(6);
-    var url = createurl(43.612966,13.50526,43.611856,13.504794);
-    var data = createJsonData(1873976112,1873976113,43.61264,13.504987)
- 	  var expetedLenght=6
-    var req = createReroutingRequest(url,data)
-      .done(function (json) {
-        var info, paths, path;
-
-        info = json.info;
-        paths = json.paths;
-        console.log(json);
-        ok(info, 'request completed in: ' + info.took);
-        ok(paths && paths.length && paths.length === 1, 'paths valid');
-        path = paths[0];
-        ok(path, 'path found');
-
-        ok(!path.points_encoded, 'points are not encoded');
-        var points = path.points.coordinates;
-        ok(points, 'data: ' + _(points).map(formatCoord).join(','));
-
-        var instructions = path.instructions;
-        ok(instructions && instructions.length === 6, 'instructions: ' + _(instructions).map(formatInstruction).join(','));
-      });
-  });
+	asyncTest("re-routing request - via mamiani", function() {
+		expect(6);
+		var url = createurl(43.612966, 13.50526, 43.611856, 13.504794);
+		var data = createJsonData(1873976112, 1873976113, 43.61264, 13.504987)
+		var expetedLenght = 6
+		var req = createReroutingRequest(url, data).done(
+			function(json) {
+				var info, paths, path;
+				info = json.info;
+				paths = json.paths;
+				ok(info, 'request completed in: ' + info.took);
+				ok(paths && paths.length && paths.length === 1,'paths valid');
+				path = paths[0];
+				ok(path, 'path found');
+				ok(!path.points_encoded, 'points are not encoded');
+				var points = path.points.coordinates;
+				ok(points, 'data: '+ _(points).map(formatCoord).join(','));
+				var instructions = path.instructions;
+				ok(instructions && instructions.length === 6,'instructions: '+ _(instructions).map(formatInstruction).join(','));
+			});
+	});
   
-  asyncTest("default routing - via santo stefano", function () {
-	    expect(6);
-	    var url = createurl(43.6125,13.515142,43.613634,13.513473);
-	    var expetedLenght=5
-	    
-	    var req = createDefaultRequest(url)
-				.done(function (json) {
-				  var info, paths, path;
 
-	        info = json.info;
-					paths = json.paths;
-					console.log(json);
-				  ok(info, 'request completed in: ' + info.took);
-				  ok(paths && paths.length && paths.length === 1, 'paths valid');
-				  path = paths[0];
-				  ok(path, 'path found');
+	asyncTest("default routing - via santo stefano", function() {
+		expect(6);
+		var url = createurl(43.6125, 13.515142, 43.613634, 13.513473);
+		var expetedLenght = 5
+		var req = createDefaultRequest(url).done(
+			function(json) {
+				var info, paths, path;
+				info = json.info;
+				paths = json.paths;
+				ok(info, 'request completed in: ' + info.took);
+				ok(paths && paths.length && paths.length === 1,'paths valid');
+				path = paths[0];
+				ok(path, 'path found');
+				ok(!path.points_encoded, 'points are not encoded');
+				var points = path.points.coordinates;
+				ok(points, 'data: '+ _(points).map(formatCoord).join(','));
+				var instructions = path.instructions;
+				ok(instructions&& instructions.length === expetedLenght,'instructions: '+ _(instructions).map(formatInstruction).join(','));
+			});
+	});
+	
+	asyncTest("re-routing request - via santo stefano - test scadenza",function() {
+		expect(6);
+		var url = createurl(43.613634, 13.513473, 43.6125, 13.515142);
+		var data = createJsonData(470083847, 470093743, 43.612263,13.514149, -10)
+		var expetedLenght = 5
+		var req = createReroutingRequest(url, data).done(
+			function(json) {
+				var info, paths, path;
+				info = json.info;
+				paths = json.paths;
+				ok(info, 'request completed in: ' + info.took);
+				ok(paths && paths.length && paths.length === 1,'paths valid');
+				path = paths[0];
+				ok(path, 'path found');
+				ok(!path.points_encoded, 'points are not encoded');
+				var points = path.points.coordinates;
+				ok(points, 'data: '+ _(points).map(formatCoord).join(','));
+				var instructions = path.instructions;
+				ok(instructions&& instructions.length === expetedLenght,'instructions: '+ _(instructions).map(formatInstruction).join(','));
+			});
+	});
+	
+  asyncTest("re-routing request - via santo stefano", function() {
+		expect(6);
+		var url = createurl(43.613634, 13.513473, 43.6125, 13.515142);
+		var data = createJsonData(470083847, 470093743, 43.612263, 13.514149)
+		var expetedLenght = 7
+		var req = createReroutingRequest(url, data).done(
+			function(json) {
+				var info, paths, path;
+				info = json.info;
+				paths = json.paths;
+				ok(info, 'request completed in: ' + info.took);
+				ok(paths && paths.length && paths.length === 1,'paths valid');
+				path = paths[0];
+				ok(path, 'path found');
+				ok(!path.points_encoded, 'points are not encoded');
+				var points = path.points.coordinates;
+				ok(points, 'data: '+ _(points).map(formatCoord).join(','));
+				var instructions = path.instructions;
+				ok(instructions&& instructions.length === expetedLenght,'instructions: '+ _(instructions).map(formatInstruction).join(','));
+			});
+	});
 
-				  ok(!path.points_encoded, 'points are not encoded');
-				  var points = path.points.coordinates;
-				  ok(points, 'data: ' + _(points).map(formatCoord).join(','));
-
-				  var instructions = path.instructions;
-				  ok(instructions && instructions.length ===  expetedLenght, 'instructions: ' + _(instructions).map(formatInstruction).join(','));
-				});
-	    });
-  
-  asyncTest("re-routing request - via santo stefano", function () {
-	    expect(6);
-	    var url = createurl(43.613634,13.513473,43.6125,13.515142);
-	    var data = createJsonData(470083847,470093743,43.612263,13.514149)
-	    var expetedLenght=7
-	    
-	    var req = createReroutingRequest(url,data)
-	      .done(function (json) {
-	        var info, paths, path;
-
-	        info = json.info;
-	        paths = json.paths;
-	        console.log(json);
-	        ok(info, 'request completed in: ' + info.took);
-	        ok(paths && paths.length && paths.length === 1, 'paths valid');
-	        path = paths[0];
-	        ok(path, 'path found');
-
-	        ok(!path.points_encoded, 'points are not encoded');
-	        var points = path.points.coordinates;
-	        ok(points, 'data: ' + _(points).map(formatCoord).join(','));
-
-	        var instructions = path.instructions;
-	        ok(instructions && instructions.length === expetedLenght, 'instructions: ' + _(instructions).map(formatInstruction).join(','));
-	      });
-	  });
-
-
-  asyncTest("re-routing request - via panoramica - worst case scenario", function () {
-	    expect(6);
-	    var url = createurl(43.619592,13.527297,43.618881,13.523998);
-	    var data = createJsonData(471237247,137545680,43.619064,13.525956)
-	 	  var expetedLenght=2
-	    var req = createReroutingRequest(url,data)
-	      .done(function (json) {
-	        var info, paths, path;
-
-	        info = json.info;
-	        paths = json.paths;
-	        console.log(json);
-	        ok(info, 'request completed in: ' + info.took);
-	        ok(paths && paths.length && paths.length === 1, 'paths valid');
-	        path = paths[0];
-	        ok(path, 'path found');
-
-	        ok(!path.points_encoded, 'points are not encoded');
-	        var points = path.points.coordinates;
-	        ok(points, 'data: ' + _(points).map(formatCoord).join(','));
-
-	        var instructions = path.instructions;
-	        ok(instructions && instructions.length === expetedLenght, 'instructions: ' + _(instructions).map(formatInstruction).join(','));
-	      });
-	  });
-
-
+	asyncTest("re-routing request - via panoramica - worst case scenario", function() {
+		expect(6);
+		var url = createurl(43.619592, 13.527297, 43.618881, 13.523998);
+		var data = createJsonData(471237247, 137545680, 43.619064,13.525956)
+		var expetedLenght = 2
+		var req = createReroutingRequest(url, data).done(
+			function(json) {
+				var info, paths, path;
+				info = json.info;
+				paths = json.paths;
+				ok(info, 'request completed in: ' + info.took);
+				ok(paths && paths.length && paths.length === 1,'paths valid');
+				path = paths[0];
+				ok(path, 'path found');
+				ok(!path.points_encoded, 'points are not encoded');
+				var points = path.points.coordinates;
+				ok(points, 'data: '+ _(points).map(formatCoord).join(','));
+				var instructions = path.instructions;
+				ok(instructions&& instructions.length === expetedLenght,'instructions: '+ _(instructions).map(formatInstruction).join(','));
+			});
+	});
 });
